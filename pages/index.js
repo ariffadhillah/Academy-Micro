bcb8eb3c66081fa81ebc281f5c29e0c10d49ff93
arif fadhillah <ariffadhillah319@gmail.com>
2021-03-26 11:08:32 -0400

    done/succes/random

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import Head from 'next/head'
-
+import Head from 'next/head';
+import Link from 'next/link'
+// npm i --save-dev autoprefixer postcss-cli tailwindcss
+// "watch:css": "postcss --watch tailwindcss/style.css -o src/assets/css/style.css" 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container mx-auto mt-4">
       <Head>
         <title>App Micro</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        saya halaman utama
+        <h1>saya halaman tama</h1>
+        <Link href="/random">
+        <a> Bring me to</a>
+        </Link>
       </main>
       
     </div>
