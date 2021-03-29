10297fd0586058a0a9c2bd6a761382eacedf1ef4
arif fadhillah <ariffadhillah319@gmail.com>
2021-03-29 01:55:25 -0400

    Dynamic Routing step 1

--- pages/random/[id].js
+++ pages/random/[id].js
@@ -1,15 +1,30 @@
+import Head from "next/head";
+import Link from "next/link";
+
+
 import React from 'react'
 
-function Todo() {
+function Todo({data}) {
+    // console.log(data)
     return (
-        <div>
-            
-        </div>
-    )
+       <>
+        <Head>
+            <title>Micro | Random | {data.title} </title>
+        </Head>
+
+        <main className="container mt-12 mx-auto">
+            <h1 className="text-3xl">{data.title}</h1>
+            <a>Please complate your task</a><br></br>
+            <Link href="/random">
+                <a>Bring me to random page</a>
+            </Link>
+        </main>
+       </>
+    );
 }
 
 Todo.getInitialProps = async (props) => {
-    console.log(props)
+    // console.log(props.query)
     const {id} = props.query
     try{
         const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
@@ -19,4 +34,4 @@ Todo.getInitialProps = async (props) => {
     }catch (error) {}
 };
 
-export default Random;
+export default Todo;
