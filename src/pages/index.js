ac4a2898b45c6d6cc939c6877513debc3f654682
arif fadhillah <ariffadhillah319@gmail.com>
2021-04-19 08:03:21 -0400

    header

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Link from 'next/link'
 
 import axios from "src/configs/axios";
 
+import Header from 'src/parts/Header';
+
 import Circle from "public/images/circle-accent-1.svg";
 // npm i --save-dev autoprefixer postcss-cli tailwindcss
 // "watch:css": "postcss --watch tailwindcss/style.css -o src/assets/css/style.css" 
@@ -10,19 +12,23 @@ import Circle from "public/images/circle-accent-1.svg";
 function Home( {data} ) {
   // console.log(props)
   return (
-    <div className="container mx-auto mt-4">
+    <>
       <Head>
         <title>App Micro</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <section className="pt-10">
+        <section className="header-clipping pt-10">
         <Circle className="absolute left-0 bottom-0"></Circle>
+        <div className="sunshine"></div>
+        <div className="container mx-auto">
+          <Header onLight></Header>
+        </div>
         </section>
       </main>
       
-    </div>
+    </>
   );
 }
 
