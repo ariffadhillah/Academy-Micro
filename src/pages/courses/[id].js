94d1e2f8cb6d5a37ab7b1d19f583426bf1159f71
arif fadhillah <ariffadhillah319@gmail.com>
2021-05-23 14:08:07 -0400

    setup detail page

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -1,37 +1,31 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import courses from "src/constants/api/courses"
 
 import React from 'react'
 
-function Todo({data}) {
+function DetailCourse({data}) {
     // console.log(data)
     return (
        <>
         <Head>
-            <title>Micro | Random | {data.title} </title>
+            <title>Micro</title>
         </Head>
-
-        <main className="container mt-12 mx-auto">
-            <h1 className="text-3xl">{data.title}</h1>
-            <a>Please complate your task</a><br></br>
-            <Link href="/random">
-                <a>Bring me to random page</a>
-            </Link>
-        </main>
+        <section className="pt-10 relative overflow-hidden" style={{height: 660}}>
+            
+        </section>
        </>
     );
 }
 
-Todo.getInitialProps = async (props) => {
+DetailCourse.getInitialProps = async (props) => {
     // console.log(props.query)
     const {id} = props.query
     try{
-        const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-            .then((response) => response.json())
-            .then((json) => json);
-    return { data };
+        const data = await courses.details(id)
+          return { data:data.data };
     }catch (error) {}
 };
 
-export default Todo;
+export default DetailCourse;
