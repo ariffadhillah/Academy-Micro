e7eef51cd8a91f2a6e4a5da5e4d21313a30858d8
arif fadhillah <ariffadhillah319@gmail.com>
2021-05-25 13:27:41 -0400

    setup Video background

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -3,29 +3,49 @@ import Link from "next/link";
 
 import courses from "src/constants/api/courses"
 
-import React from 'react'
+import Youtube from 'react-youtube'
 
 function DetailCourse({data}) {
-    // console.log(data)
+    console.log(data)
     return (
        <>
         <Head>
             <title>Micro</title>
         </Head>
         <section className="pt-10 relative overflow-hidden" style={{height: 660}}>
-            
+            {
+                data?.chapters?.[0]?.lessons?.[0]?.video && 
+                <div className="video-wrapper">
+                    <Youtube 
+                        videoId={data?.chapters?.[0]?.lessons?.[0]?.video}
+                        id={data?.chapters?.[0]?.lessons?.[0]?.video}
+                        opts={{
+                            playerVars: {
+                                loop: 1,
+                                mute: 1,
+                                autoplay: 1,
+                                controls: 0,
+                                showinfo: 0
+                            }
+                        }}
+                        onEnd={(event) => {
+                            event.target.playVideo();
+                        }}
+                    ></Youtube>
+                </div>
+            }                
         </section>
        </>
     );
 }
 
 DetailCourse.getInitialProps = async (props) => {
-    // console.log(props.query)
-    const {id} = props.query
-    try{
-        const data = await courses.details(id)
-          return { data:data.data };
-    }catch (error) {}
-};
+    const { id } = props.query;
+    try {
+      const data = await courses.details(id);
+  
+      return { data };
+    } catch (error) {}
+  }; 
 
-export default DetailCourse;
+  export default DetailCourse;
