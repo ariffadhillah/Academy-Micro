b8f079f4a5239c3be64748e1220e07aa5c298348
arif fadhillah <ariffadhillah319@gmail.com>
2021-06-10 05:38:44 -0400

    CSSTransition/sticy

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -1,5 +1,10 @@
+import {useEffect, useRef, useState} from 'react'
+
 import Head from "next/head";
 import Link from "next/link";
+
+import { CSSTransition } from 'react-transition-group'
+
 
 import Header from 'src/parts/Header'
 
@@ -17,10 +22,28 @@ import Nametag from 'public/images/icon-nametag.svg'
 import Playback from 'public/images/icon-playback.svg'
 
 import Certificate from 'public/images/icon-certificate.svg'
+import Footer from 'src/parts/Footer';
 
 
 function DetailCourse({data}) {
-    console.log(data)
+    console.log(data);
+    const footer = useRef (null);
+
+    const [isSticky, setisSticky] = useState(() => true);
+    useEffect(() => {
+            const stickyOffsetTop = footer.current.getBoundingClientRect().top;
+
+            const stickyMetaToggler = () => {
+                console.log(stickyOffsetTop, window.pageYOffset,window.innerHeight ,stickyOffsetTop >= window.pageYOffset + window.innerHeight);
+                setisSticky(stickyOffsetTop >= window.pageYOffset + window.innerHeight);
+            };
+            
+            window.addEventListener("scroll", stickyMetaToggler);
+            
+        return () => {
+            window.removeEventListener("scroll", stickyMetaToggler);
+        };
+    }, []);
     return (
        <>
         <Head>
@@ -84,6 +107,51 @@ function DetailCourse({data}) {
                     </div>
                 </div>
             </div>
+            <div>
+            <CSSTransition
+            in={isSticky}
+            timeout={300}
+            classNames="meta-price"
+            unmountOnExit
+          >
+            <div className="meta-price w-full bg-white z-10 left-0 md:py-3">
+              <div className="w-full md:w-3/4 md:mx-auto">
+                <div className="flex items-center">
+                  <div className="w-full ml-2 md:mx-auto">
+                    <h2 className="text-gray-600 text-xs md:text-base">
+                      Nama Kelas
+                    </h2>
+                    <h3 className="text-base md:text-2xl text-gray-900">
+                      {data?.name ?? "Nama Kelas"}
+                    </h3>
+                  </div>
+                  <h5 className="text-base md:text-2xl text-teal-500 whitespace-no-wrap mr-4">
+                    {/* {data?.type === "free" ? ( "Free" ) : (<span>Rp {formatThousand(data?.price ?? 0)}</span> )} */}
+                  </h5>
+                  <a
+                    href={`${process.env.NEXT_PUBLIC_MEMBERPAGE_URL}/joined/${data.id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-orange-500 hover:bg-orange-400 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 whitespace-no-wrap" style={{width: 160, textAlign: 'center'}}
+                  >
+                    {data?.type === "free" ? "Enroll Now" : "Buy Now"}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </CSSTransition>
+            </div>
+            <div className="w-3/4 mx-auto mt-8">
+                <div className="w-3/4">
+                    <section>
+                        <h6 className="font-medium text-gray-900 text-2xl mb-4">About <span className="text-teal-500">Courses</span></h6>
+                        <p className="text-gray-600 text-lg leading-relaxed mb-3">{data?.description ?? "no Description Found"}</p>
+                    </section>
+                </div>
+            </div>
+        </section>
+        <section className="mt-24 bg-indigo-900 py-12" ref={footer}>
+            <Footer></Footer>
         </section>
        </>
     );
